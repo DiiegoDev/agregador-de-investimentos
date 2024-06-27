7424d220876c303ec85797ba23480e3a2bf39262
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-06-27 18:00:21 -0300

    feat: create UserContoller

---
+++ src/main/java/tech/project/agregadordeinvestimentos/controllers/UserController.java
@@ -0,0 +1,38 @@
+package tech.project.agregadordeinvestimentos.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.project.agregadordeinvestimentos.entities.User;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @PostMapping
+  public ResponseEntity<User> create(@RequestBody User user) {
+    return null;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<User>> findAll() {
+    return null;
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> findOneById(@PathVariable("id") UUID id) {
+    return null;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Void> updateOneById(@PathVariable("id") UUID id) {
+    return null;
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteOneById(@PathVariable("id") UUID id) {
+    return null;
+  }
+}
