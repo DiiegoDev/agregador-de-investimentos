4bcbdb38876e15cf9a5207d2e44a06947932a71f
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-06-28 15:18:06 -0300

    feat: add findUserByEmail in UserRespository

--- src/main/java/tech/project/agregadordeinvestimentos/repositories/UserRespository.java
+++ src/main/java/tech/project/agregadordeinvestimentos/repositories/UserRespository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.project.agregadordeinvestimentos.entities.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRespository extends JpaRepository<User, UUID> {
+  Optional<User> findUserByEmail(String email);
 }
